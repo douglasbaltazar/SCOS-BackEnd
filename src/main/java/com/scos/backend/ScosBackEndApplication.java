3588a4598021fb88b54292d6f9fa14611272d606
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-01-16 15:01:39 -0300

    #1 Init project

---
+++ src/main/java/com/scos/backend/ScosBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.scos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScosBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scos/backend/ScosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScosBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
